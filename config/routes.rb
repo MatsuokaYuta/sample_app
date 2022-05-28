56e98d8fabf71fab22c18167c7f00f461446b3d1
MatsuokaYuta <yu-matsuoka@wiz-tech.jp>
2022-05-28 02:16:45 +0000

    Create PostEditPage

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get 'posts/index' => "posts#index"
   get "posts/new" => "posts#new"
-  post "posts/create" => "posts#create"
-  
   get "posts/:id" => "posts#show"
-
+  post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  
   get '/' => "home#top"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
