c01370cdddf03c720f2b6862e6c682246c0bbdae
MatsuokaYuta <yu-matsuoka@wiz-tech.jp>
2022-05-29 14:58:04 +0000

    Create UserShowPage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def index
     @users = User.all.order(created_at: :desc)
   end
+  
+  def show
+    @user = User.find_by(id: params[:id])
+  end
 end
