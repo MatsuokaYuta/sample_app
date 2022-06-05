c21c5018751c2982404a0fbb55c4c18e90d91ab4
MatsuokaYuta <yu-matsuoka@wiz-tech.jp>
2022-06-05 02:48:45 +0000

    Show contributors in post list

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220602032316) do
+ActiveRecord::Schema.define(version: 20220605022247) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
